1c641e7e2f3870f97cc97abecf1f2a26ed68e8c3
aryan <guptaaryan92136@gmail.com>
2026-02-10 13:31:18 +0530

    enhanced error handling in the data layer

--- app/src/main/java/com/example/weather/HomeScreen/data/remote/Repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/weather/HomeScreen/data/remote/Repository/WeatherRepositoryImpl.kt
@@ -1,20 +1,24 @@
 package com.example.weather.HomeScreen.data.remote.Repository
 
 import com.example.weather.HomeScreen.data.remote.Api.WeatherApi
+import com.example.weather.HomeScreen.data.remote.Mapper.Result
+import com.example.weather.HomeScreen.data.remote.Mapper.mapToWeatherError
 import com.example.weather.HomeScreen.data.remote.Mapper.toDomain
 import com.example.weather.HomeScreen.domain.model.Weather
+import com.example.weather.HomeScreen.domain.model.WeatherError
 import com.example.weather.HomeScreen.domain.repository.WeatherRepository
+import io.ktor.client.plugins.ClientRequestException
 
 class WeatherRepositoryImpl(
     private val api: WeatherApi
 ) : WeatherRepository {
 
-    override suspend fun getCurrentWeather(city: String): Result<Weather> {
+    override suspend fun getCurrentWeather(city: String): Result<Weather , WeatherError> {
         return try {
             val response = api.getWeatherApi(city)
-            Result.success(response.toDomain())
-        } catch (e: Exception) {
-            Result.failure(e)
+            Result.Success(response.toDomain())
+        } catch (e: ClientRequestException) {
+            Result.Error(mapToWeatherError(e))
         }
     }
 }
